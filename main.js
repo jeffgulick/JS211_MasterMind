d39721913b91d6a9fd60f2ab7abdcd943b9729c3
JEFFGULICK <jeffgulick@gmail.com>
2020-07-19 01:43:10 -0500

    almost finished

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
+var colors = require('colors');
 let board = [];
 let solution = '';
 let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
@@ -17,30 +18,84 @@ const printBoard = () =>  {
   }
 }
 
+//generates 4 random letters 
 const generateSolution = () =>  {
   for (let i = 0; i < 4; i++) {
     const randomIndex = getRandomInt(0, letters.length);
     solution += letters[randomIndex];
   }
-}
 
+  console.log(solution)
+}
+//random numbers hhb
 const getRandomInt = (min, max) => {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-const generateHint = () =>  {
-  // your code here
+const generateHint = (guess) =>  {
+  let solutionArray = [];
+  let guessArray = [];
+  let finalArr = [];
+  let correctLetterLocations = 0;
+  let correctLetters = 0;
+
+  solutionArray = solution.split('');
+  guessArray = guess.split('');
+
+//checks for correct letters in correct position
+  for(let i =0; i<solutionArray.length; i++){
+    if(solutionArray[i] === guessArray[i]){
+      finalArr.push(guessArray[i]);
+      correctLetterLocations ++;
+      solutionArray[i] = null;
+    }
+  }
+
+//checks for correct letters at any position
+for(let i = 0; i<solutionArray.length; i++){
+  if(guessArray.indexOf(solutionArray[i]) != - 1){
+    correctLetters ++;
+    solutionArray[i] = null;
+  }
+}
+let hint = colors.red(correctLetters) + ' - ' + correctLetterLocations;
+
+return hint;
 }
 
 const mastermind = (guess) => {
-  solution = 'abcd'; // Comment this out to generate a random solution
-  // your code here
+  if (guess != solution){
+   let hint = generateHint(guess);
+    console.log("Current Hint: " + hint)
+    board.push(hint)
+  }
+
+  else if (guess == solution){
+    console.log('You guessed it!!!!');
+    console.log('Time for a new game.')
+    console.log('START GUESSING')
+    resetGame();
+    return "You guessed it!";
+}
 }
 
+const resetGame = () => {
+  board = [];
+  solution = '';
+  generateSolution();
+}
+
+const checkTurn = () => {
+  if(board.length == 10){
+    console.log('Sorry you are out of turns. Time for a new game.');
+    resetGame();
+  }
+}
 
 const getPrompt = () =>  {
   rl.question('guess: ', (guess) => {
     mastermind(guess);
+    checkTurn();
     printBoard();
     getPrompt();
   });
